3266f505ad2766173f00c227604f4575816254a5
Riju Vijayan <rijuvijayan@gmail.com>
2025-11-25 15:45:43 +0530

    chore: fix tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -16,6 +16,7 @@ describe("Items API", () => {
       { id: 2, name: "Item 2" },
       { id: 3, name: "Item 3" },
       { id: 4, name: "Item 4" },
+      { id: 5, name: "Item 5" },
     ]);
   });
 
